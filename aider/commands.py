5a0ad02580042f45f99eb27cc26e6c165b23a895
Ross Packard <ross.packard@gmail.com>
2025-03-12 21:40:28 -0400

    Feature: Command /add-dirty which adds all modified and untracked files to context

--- aider/commands.py
+++ aider/commands.py
@@ -1621,6 +1621,18 @@ Just show me the edits I need to make.
         for completion in all_completions:
             yield completion
 
+    def cmd_add_dirty(self, args):
+        "Add all dirty files in the repository to the chat"
+        if not self.coder.repo:
+            self.io.tool_error("No git repository found.")
+            return
+
+        dirty_files = self.coder.repo.get_dirty_files()
+        untracked_files = self.coder.repo.repo.untracked_files
+        dirty_files.extend(untracked_files)
+        for file in dirty_files:
+            self.cmd_add(file)
+
 def expand_subdir(file_path):
     if file_path.is_file():
         yield file_path
